69933aee43b7ef79beb6b5dad3ffe91368f2a852
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-09-05 21:05:40 +0530

    vpc

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
     local.common_tags,
     {
         #roboshop-dev-public-us-east-la
-        Name = "${var.project}-${var.environment}-public-${data.aws_availability_zones.available.names[count.index]}"
+        Name = "${var.project}-${var.environment}-public-${aws_availability_zones.available.names}te"
     },
     var.subnet_tags
     )
