0e07ad4f0c7675af2b3984109084fad674c305a1
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-09-05 21:45:16 +0530

    vpc

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "ID of project VPC"
   value       = aws_vpc.main.id
 }
+
+output "aws_availability_zones" {
+  description = "List of availability zones"
+  value       = data.aws_availability_zones.available.names
+}
