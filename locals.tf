f260a0f7d97b992b6f12d0578b1e5aa80ea0c131
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-09-07 12:32:09 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -70,5 +70,11 @@ locals {
 
 
     ####nat_gateway_tags####
-   nat_gateway_final_tags = merge(local.common_tags, { Name = "${var.project}-${var.environment}-nat-gateway" }, var.nat_gateway_tags)
+    nat_gateway_final_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-nat-gateway"
+    },
+    var.nat_gateway_tags
+    )
 }
