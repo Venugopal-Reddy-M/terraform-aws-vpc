545220d80d79c2c60e666d915d4d7026ab569cea
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-09-08 23:48:44 +0530

    vpc

--- peering-vpc.tf
+++ peering-vpc.tf
@@ -3,6 +3,7 @@ resource "aws_vpc_peering_connection" "default" {
   #peer_owner_id = var.peer_owner_id
   #accepter
   peer_vpc_id   = data.aws_vpc.default.id
+  #requester/my-vpc/our-vpc
   vpc_id        = aws_vpc.main.id
 
   accepter {
@@ -15,4 +16,21 @@ resource "aws_vpc_peering_connection" "default" {
   auto_accept = true
 
   tags = local.vpc_peering_final_tags
+}
+
+
+# Create a route/roboshop-dev-default
+resource "aws_route" "public_to_peer" {
+    count = var.is_peering_enabled ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc_peering_connection.default.cidr_block
+  vpc_peering_connection_id = data.aws_vpc_peering_connection.default[count.index].id
+}
+
+# Create a route/default-to-roboshop-dev
+resource "aws_route" "peer_to_public" {
+    count = var.is_peering_enabled ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = data.aws_vpc_peering_connection.default[count.index].id
 }
