c833fd9e0339307ce033d520983185bb7f37bd41
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-09-10 22:35:18 +0530

    vpc

--- peering-vpc.tf
+++ peering-vpc.tf
@@ -20,17 +20,32 @@ resource "aws_vpc_peering_connection" "default" {
 
 
 # Create a route/roboshop-dev-default
-resource "aws_route" "public_to_peer" {
+resource "aws_route" "public_peering" {
     count = var.is_peering_enabled ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
+resource "aws_route" "private_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
 # Create a route/default-to-roboshop-dev
-resource "aws_route" "peer_to_public" {
+resource "aws_route" "default_peering" {
     count = var.is_peering_enabled ? 1 : 0
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
+
